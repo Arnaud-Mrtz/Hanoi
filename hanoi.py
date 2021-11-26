893980108e9df6c5844413b2cb4a27bcbf3b54d2
Arnaud <mrtz.arnaud@gmail.com>
2021-11-26 11:28:54 +0100

    azertyui

---
+++ hanoi.py
@@ -0,0 +1,11 @@
+# MARTINEZ Arnaud
+# Importation de pygame et initialisation du module
+import pygame
+from partie import Partie
+pygame.init()
+
+
+# Création de la fenêtre ( titre et dimentions de la fenêtre)
+pygame.display.set_caption("Pong!")
+screen = pygame.display.set_mode((1080, 720))
+
