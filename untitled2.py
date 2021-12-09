d9e48e26dd1aba26c92119b40cc7ed0a90b46a7d
Thingkim <78851291+Thingkim@users.noreply.github.com>
2021-12-09 11:49:20 +0100

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -1,7 +1,6 @@
 class Domino:
 	def __init__(self, face_a, face_b):
-		white = self.white(face_a, face_b)
-		double = self.double(face_a, face_b)
+		white, double = self.white(face_a, face_b),self.double(face_a, face_b)
 		self.state(white, double, face_a, face_b)
 	def white(self, face_a, face_b):
 		if face_a == 0 or face_b == 0:
@@ -11,12 +10,32 @@ class Domino:
 			return True
 	def state(self, white, double, face_a, face_b):
 		if white and double:
-			print("Votre Domino : [{}-{}]".format(face_a, face_b),"est un double blanc")
+			print("""Votre Domino : 
+╔═════╗
+║  {}  ║
+║  -  ║
+║  {}  ║
+╚═════╝""".format(face_a, face_b),"""
+est un double blanc""")
 		elif white:
-			print("Votre Domino : [{}-{}]".format(face_a, face_b),"possède un côté blanc")
+			print("""Votre Domino : 
+╔═══════╗
+║ {} - {} ║
+╚═══════╝""".format(face_a, face_b),"""
+possède un côté blanc""")
 		elif double:
-			print("Votre Domino : [{}-{}]".format(face_a, face_b),"est un double")
+			print("""Votre Domino : 
+╔═════╗
+║  {}  ║
+║  -  ║
+║  {}  ║
+╚═════╝""".format(face_a, face_b),"""
+est un double""")
 		else:
-			print("Votre Domino : [{}-{}]".format(face_a, face_b),"n'est ni double et ne possède pas de blanc")
+			print("""Votre Domino : 
+╔═══════╗
+║ {} - {} ║
+╚═══════╝""".format(face_a, face_b),"""
+n'est ni double et ne possède pas de blanc""")
 		print("Sa valeur est de",str(face_a+face_b))
 Domino(int(input("1re face du Domino : ")), int(input("2e face du Domino : ")))
